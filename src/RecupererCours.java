49d1cae79025644a7acdced516623b674abe2816
stylnoox <stylnoox@gmail.com>
2015-07-07 03:31:47 +0200

    OK

--- src/RecupererCours.java
+++ src/RecupererCours.java
@@ -1,11 +1,24 @@
 package src;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import net.sf.opencsv;
+
+import com.opencsv.CSVReader;
 
 public class RecupererCours
 {
@@ -18,6 +31,7 @@ public class RecupererCours
 		this.s = s;
 		this.startDate = startDate;
 		System.out.println(genererAdresse());
+		getCours();
 		// getHistoricalPrices(genererAdresse());
 
 	}
@@ -25,14 +39,46 @@ public class RecupererCours
 	/**
 	 * @return Une MAP associant chaque cours de l'action de s à son cours
 	 */
+	@SuppressWarnings("resource")
 	public Map<Date, Double> getCours()
-	{
-		File file = new File(genererAdresse());
-		FileReader fr = new FileReader(file);
-		
-		CSVReader csvReader = new CSVReader(fr, SEPARATOR);
-
-		return null;
+	{	
+		Map<Date, Double> map = new HashMap<Date, Double>();
+		try
+		{
+//			Generation url
+			URL url = new URL(genererAdresse());
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			CSVReader cr = new CSVReader(in);
+			
+			List<String[]> ls = cr.readAll();
+			ls.remove(0);
+			
+//			Recuperation date et close
+			for (String[] line : ls)
+			{
+				Date d=new Date(1900, 1, 1);
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				String sd = line[0];
+				try
+				{
+					d = sdf.parse(sd);
+				} catch (ParseException e1)
+				{
+					System.out.println("Format date incorrect : YYYY-MM-DD attendu !");
+					e1.printStackTrace();
+				}
+				map.put(d, Double.parseDouble(line[4]));
+				
+			}
+			
+			
+			
+		} catch (IOException e1)
+		{
+			System.out.println("Ouverture du fichier impossible");
+			 
+		}
+			return map;
 	}
 
 	@SuppressWarnings("deprecation")
